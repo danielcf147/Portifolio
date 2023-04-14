a89c9be11635536200b3f151e0e6ede31a796e00
= <danielcf.vix@hotmail.com>
2023-04-14 09:54:00 -0300

    rev0

---
+++ src/utils/stackData.ts
@@ -0,0 +1,39 @@
+import imgHTML from "../public/static/img/stack/html.svg";
+import imgCSS from "../public/static/img/stack/css.svg";
+import imgJS from "../public/static/img/stack/js.svg";
+import imgNode from "../public/static/img/stack/node.svg";
+import imgStyled from "../public/static/img/stack/styled.svg";
+import imgReact from "../public/static/img/stack/react.svg";
+import { FaPython, FaVuejs } from "react-icons/fa";
+import { SiDjango, SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: imgHTML,
+  },
+  {
+    title: "CSS",
+    img: imgCSS,
+  },
+  {
+    title: "JS",
+    img: imgJS,
+  },
+  {
+    title: "Node JS",
+    img: imgNode,
+  },
+  {
+    title: "Styled Components",
+    img: imgStyled,
+  },
+  {
+    title: "React",
+    img: imgReact,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+  nameUser: "Daniel Campos França",
+  githubUser: "danielcf147",
+  linkedinUser: "danielcf147",
+  whatsappNumber: "+5527998250957",
+};
